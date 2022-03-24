9624348c42660a13058cefbd375563b353b22e63
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2022-03-24 15:09:24 +0530

    added favicon , improved css

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const baseUrl = "https://api.npoint.io/20c1afef1661881ddc9c";
 
-
 let playerAllData;
 let filterItem;
-let SearchText=""
+let SearchText = "";
 
+// fecth the data from from api
 async function allFootballData() {
   let res = await fetch(baseUrl);
   let data = await res.json();
   // console.log(data.playerList);
-     playerAllData = data.playerList;
+  playerAllData = data.playerList;
 
   //Sort function
   playerAllData.sort(function (a, b) {
@@ -22,11 +22,9 @@ async function allFootballData() {
 allFootballData();
 let main_div = document.getElementById("football_div");
 
+/// diplay the data players data on page
 function displayFootball(allData) {
   allData.forEach((single) => {
-    
-
-
     let image = document.createElement("img");
     image.src = "./player-images/" + `${single.Id}` + ".jpg";
     image.alt = single.PFName;
@@ -41,12 +39,12 @@ function displayFootball(allData) {
     skillName.innerText = "Skill - " + single.SkillDesc;
 
     let value = document.createElement("div");
-    value.innerText = "Value - $" + single.Value;
+    value.innerText = "Value - $M " + single.Value;
 
-    let upcomingMatch = document.createElement("div");
+    let upcomingMatch = document.createElement("marquee");
     upcomingMatch.innerText = `Upcoming Match - ${single.UpComingMatchesList[0].CCode} VS ${single.UpComingMatchesList[0].VsCCode}`;
 
-    let time = document.createElement("div");
+    let time = document.createElement("marquee");
     time.innerText = converTime(single.UpComingMatchesList[0].MDate);
 
     single.UpComingMatchesList[0].CCode == ""
@@ -54,9 +52,11 @@ function displayFootball(allData) {
       : div.append(image, playName, skillName, value, upcomingMatch, time);
 
     main_div.appendChild(div);
+    console.log("teamname", single.TName);
   });
 }
 
+// sorting player by their value
 function sortLowToHigh() {
   playerAllData.sort(function (a, b) {
     return a.Value - b.Value;
@@ -65,37 +65,33 @@ function sortLowToHigh() {
 }
 
 sortLowToHigh();
+
+// UTC to IST
 function converTime(t) {
   let [date, time, meridian] = t.split(" ");
   // time = time.split(":")
-  time ="05:30:00"
+  time = "05:30:00";
   let res = "Match Time -" + [date, time, meridian].join(" ");
   return res;
 }
 
-/// Serach by team name
-
-
-function searchPlayer  ()  {
-   SearchText = document.getElementById("myInput").value;
+/// Serach by team name or players name
+function searchPlayer() {
+  SearchText = document.getElementById("myInput").value;
   console.log("SearchText:", SearchText);
-
-   
-   filterItem = playerAllData.filter((item) => {
- 
-    if (item.PFName == SearchText  || item.TName==SearchText) return item;
-    
+  filterItem = playerAllData.filter((item) => {
+    if (item.PFName == SearchText || item.TName == SearchText) return item;
   });
   console.log("filterItem:", filterItem);
+}
 
-  
-};
 
+//to show filerData
 function showFliterData() {
-
   if (SearchText.length == 0) {
     alert("please enter someting");
+    displayFootball(filterItem);
   }
   main_div.innerHTML = null;
-   displayFootball(filterItem);
+  displayFootball(filterItem);
 }
